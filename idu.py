94ce4bfdf9ae1cfa68bb780bdc2ba7cee2743a62
felipecdgodoy <54994053+felipecdgodoy@users.noreply.github.com>
2019-09-25 12:08:25 -0400

    Included documentation

--- idu.py
+++ idu.py
@@ -1,26 +1,33 @@
 from math import log, exp
 
 def index_pib(realPIBpercapita):
-    "Returns the index value for PIB. Takes in "
+    "Returns the index value for PIB. Takes in the value in dollars. I.e. 3451"
     return 100.0 * ((log(realPIBpercapita) - log(604)) / (log(145894) - log(604)))
 
 def index_unemployement(unemployement_rate):
-    return round((-99/22) * unemployement_rate) + 113.5
+    "Returns the index value for Unemployement Rate. Takes in a percentage such as 0.0659"
+    return round((-99/22) * (100 * unemployement_rate)) + 113.5
 
 def index_literatepopulation(literate_rate):
-    return round(((99/50) * literate_rate) - 98, 2)
+    "Return the index value for Literacy. Takes in a percentage such as 0.947"
+    return round(((99/50) * (100 * literate_rate)) - 98, 2)
 
 def index_accessToCleanWater(water_access_rate):
-    return ((99/90) * water_access_rate) - 10
+    "Returns the index value for Access to Clean Water. Takes in a percentage such as 0.982"
+    return ((99/90) * (100 * water_access_rate)) - 10
 
 def index_accessToHealth(number_doc_1000_people):
+    "Returns the index value for Access to Health, measured by the number of health professionals "
+    "per 1000 residents in the city. Takes in the raw number of doctors / 1000 people such as 10.3"
     return 100 / (1 + (99 * exp(-0.91902397 * number_doc_1000_people)))
 
 def index_lifeExpectancy(lifeExpectancy):
+    "Returns the index value for Life Expectancy. Takes in the expected age such as 77.6"
     return 100 / (1 + (36129.25286 * exp(-0.155256271 * lifeExpectancy)))
 
 def UDI(realPIBpercapita, unemployement_rate, literate_rate, water_access_rate,
         number_doc_1000_people, lifeExpectancy):
+    "Calculates the final UDI value for a city"
     return round((
                     index_pib(realPIBpercapita)
                   * index_unemployement(unemployement_rate)
@@ -32,6 +39,7 @@ def UDI(realPIBpercapita, unemployement_rate, literate_rate, water_access_rate,
 
 def describeCity(cityName, realPIBpercapita, unemployement_rate, literate_rate, water_access_rate,
         number_doc_1000_people, lifeExpectancy):
+    "Prints the description of a specific city's UDI results"
     print("Analyzing city: " + cityName)
     print("Index value for PIB: " + index_pib(realPIBpercapita))
     print("Index value for Unemployement: " + index_unemployement(unemployement_rate))
